ad5f1a2197fb48ed8a560e3d0fa8c9f037a58c6c
DowelMartin <martindowelofficial@gmail.com>
2021-02-02 14:16:43 +0100

    Orders
    60102
    GET/Orders​/GetMyOrders
    GET/Orders​/GetCarTypes

---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Order;
+
+namespace UITMBER.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetMyOrders();
+        Task<List<Order>> GetCarTypes();
+    }
+}
---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Order;
+using UITMBER.Services.Request;
+using Xamarin.Forms;
+
+namespace UITMBER.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        public IRequestService _requestService => DependencyService.Get<IRequestService>();
+
+        public async Task<List<Order>> GetMyOrders()
+        {
+            var uri = $"{Settings.SERVERENDPOINT}/Orders/GetMyOrders";
+
+            return await _requestService.GetAsync<List<Order>>(uri);
+        }
+        //Czy aby GetCarTypes nie powinno zwracać listy stringów, zamiast Orderów, tak jak w GetLuggageTypes?
+        //Odwołanie do UITMBER.Api
+        public async Task<List<Order>> GetCarTypes()
+        {
+            var uri = $"{Settings.SERVERENDPOINT}/Orders/GetCarTypes";
+
+            return await _requestService.GetAsync<List<Order>>(uri);
+        }
+    }
+}
