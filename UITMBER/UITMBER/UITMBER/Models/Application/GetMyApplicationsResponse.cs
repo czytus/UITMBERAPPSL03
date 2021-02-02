a395a9881fa0c9e282f1db6c4435aa62892fa2fc
Marcin Lipa <marcin.lipa@onet.pl>
2021-02-02 15:21:38 +0100

    Add ApplicationService
    add SendApplication method
    add GetMyApplications method

---
+++ UITMBER/UITMBER/UITMBER/Models/Application/GetMyApplicationsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UITMBER.Models.Application
+{
+    class GetMyApplicationsResponse
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public DateTime Date { get; set; }
+        public bool Accepted { get; set; }
+        public string DriverLicenceNo { get; set; }
+        public string DriverLicencePhoto { get; set; }
+        public long CarId { get; set; }
+    }
+}
