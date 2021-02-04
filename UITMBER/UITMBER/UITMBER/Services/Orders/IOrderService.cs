cc5892962b847c3e3395ed4c2e1f87f4c9f493ec
Sebastian Poweska <spoweska98@gmail.com>
2021-02-04 17:35:03 +0100

    Orders
    60068
    GET/Orders/GetClientOrderDetails
    GET/Orders/GetLuggageTypes

---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Orders;
+
+namespace UITMBER.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetClientOrderDetails();
+        Task<List<string>> GetLuggageTypes();       
+
+    }
+}
---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Orders;
+using UITMBER.Services.Request;
+using Xamarin.Forms;
+
+namespace UITMBER.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        public IRequestService _requestService => DependencyService.Get<IRequestService>();
+
+        public async Task<List<Order>> GetClientOrderDetails()
+        {
+            var uri = $"{Settings.SERVERENDPOINT}/Orders/GetClientOrderDetails";
+
+
+            return await _requestService.GetAsync<List<Order>>(uri);
+
+        }
+
+        public async Task<List<string>> GetLuggageTypes()
+        {
+            var uri = $"{Settings.SERVERENDPOINT}/Orders/GetLuggageTypes";
+
+
+            return await _requestService.GetAsync<List<string>>(uri);
+
+        }     
+
+
+    }
+}
