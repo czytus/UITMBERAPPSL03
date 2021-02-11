bac693c02eba1b00c3b49a1f3eabab9a15986a93
Marcin Lipa <marcin.lipa@onet.pl>
2021-02-11 18:29:49 +0100

    OrdersViewModel + OrdersPage

---
+++ UITMBER/UITMBER/UITMBER/Views/OrdersPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UITMBER.Views
+{
+    public partial class OrdersPage : ContentPage
+    {
+        OrdersViewModel _viewModel;
+
+        public OrdersPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new OrdersViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
